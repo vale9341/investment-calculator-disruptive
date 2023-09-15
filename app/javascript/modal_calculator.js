fd852b2b64cc1239c1cda13b4ff4bd9873c5d9ff
vale9341 <vale9341@gmail.com>
2023-09-15 09:56:11 -0600

    add funtionality to calculate investment

---
+++ app/javascript/modal_calculator.js
@@ -0,0 +1,75 @@
+class ModalCalculator {
+  constructor(modal){
+    _this = this
+    $modal = $(modal)
+    $cryptos = $modal.data("cryptos")
+    $form = $modal.find("form")
+    $tableInformation = $modal.find("#calculator-table")
+    $amountInput = $form.find("#amount")
+    $cryptoSelect = $form.find("#crypto")
+    $btnCalculate = $form.find("#btn-calculate")
+    $btnCalculate.on("click", function(){ _this.tableFilling() })
+  }
+
+  tableFilling(){
+    tbody = $tableInformation.find("tbody")
+    tbody.empty()
+    investments = this.calculateInvestment()
+    
+    investments.forEach(investment => {
+      tbody.append(this.tableRow(investment))
+    })
+  }
+
+  calculateInvestment(){
+    const months = Array.from({ length: 12 }, (_, i) => i + 1)
+    let crypto = $cryptos.find((crypto) => crypto["asset_id"] == $cryptoSelect.val())
+    let investments= []
+    let amountInitial = $amountInput.val()
+    months.forEach(month => {
+      monthlyPerformance = this.monthlyPerformance(amountInitial, month, crypto)
+      amountInitial = monthlyPerformance["capitalAcumulated"]
+      investments.push(monthlyPerformance)
+    });
+
+    return investments
+  }
+
+  monthlyPerformance(amountInitial, month, crypto){
+    porcentage = Number(crypto["monthly_percentage"]) / 100
+    potential = Math.pow(1 + porcentage, 1)
+    capitalFinal = amountInitial * potential
+    interestGenerated = capitalFinal - amountInitial
+    cryptoPrice = crypto["rate"]
+    amountInCrypto = capitalFinal / crypto["rate"]
+    return {
+      month: month,
+      amountInitial: amountInitial,
+      monthlyPercentage: crypto["monthly_percentage"],
+      interestGenerated: interestGenerated,
+      capitalAcumulated: capitalFinal,
+      amountInCrypto: amountInCrypto
+    }
+  }
+
+  tableRow(investment){
+    return $("<tr>" +
+        `<td>${investment["month"]}</td>` +
+        `<td>$${ new Intl.NumberFormat('en-US').format(investment['amountInitial']) }</td>` +
+        `<td>%${ new Intl.NumberFormat('en-US').format(investment['monthlyPercentage']) }</td>` +
+        `<td>$${ new Intl.NumberFormat('en-US').format(investment['interestGenerated']) }</td>` +
+        `<td>$${ new Intl.NumberFormat('en-US').format(investment['capitalAcumulated']) }</td>` +
+        `<td>${ investment['amountInCrypto'] }</td>` +
+      "</tr>")
+  }
+
+  findCrypto(asset_id){
+    return $cryptos.find((crypto) => crypto["asset_id"] == asset_id)
+  }
+}
+
+document.addEventListener("turbolinks:load", function() {
+  $("#modal-calculator").each(function(){
+    new ModalCalculator(this)
+  })
+})
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,4 @@ ActiveStorage.start()
 
 import "bootstrap"
 import "../stylesheets/application"
+import "../modal_calculator"
